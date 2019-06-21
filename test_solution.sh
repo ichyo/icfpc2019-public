e1ae5b1ed442a7b33e6a7b7f9478b62fae1c1d44
ichyo <ichyo00@gmail.com>
2019-06-22 00:36:42 +0900

    Update script

--- test_solution.sh
+++ test_solution.sh
@@ -2,10 +2,12 @@
 
 set -eu
 
+LOG_FILE=test.log
 BASEDIR=$(dirname "$0")
 cd $BASEDIR
 
-echo "Building main"
+echo "$(date +"%Y-%m-%d %T"): Running test" | tee -a $LOG_FILE
+echo "$(date +"%Y-%m-%d %T"): Building main" | tee -a $LOG_FILE
 cargo build --release
 
 OUTPUT="./solutions/test"
@@ -14,4 +16,5 @@ echo "Creating $OUTPUT"
 mkdir -p $OUTPUT
 
 ./target/release/icfpc2019 --output $OUTPUT --input ./input
-cargo run --bin score --release -- --input ./input --output $OUTPUT
+
+cargo run --bin score --release -- --input ./input --output $OUTPUT | tee -a $LOG_FILE
