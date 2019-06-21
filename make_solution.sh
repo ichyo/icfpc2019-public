5478a07f9eec3d50dc241bef3d67195c7d15d769
ichyo <ichyo00@gmail.com>
2019-06-21 20:50:49 +0900

    change solution script

--- make_solution.sh
+++ make_solution.sh
@@ -9,12 +9,8 @@ OUTPUT="./solutions/$(date +"%m%d")/$(date +"%H")/$(date +"%M%S")"
 
 echo "Creating $OUTPUT"
 mkdir -p $OUTPUT
-export OUTPUT
 
-echo "running..."
-for f in ./input/prob-*.desc; do
-    ./solve_single.sh $f $OUTPUT
-done
+./target/release/icfpc2019 --output $OUTPUT --input ./input
 
 echo "Creating zip $OUTPUT/solutions.zip"
 cd $OUTPUT
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use clap::{App, Arg};
+use glob::glob;
+use std::fs::File;
+use std::io::Write;
+
+fn find_files(input_root: &str) -> Vec<String> {
+    glob(&format!("{}/prob-*.desc", input_root))
+        .expect("glob pattern")
+        .map(|p| {
+            p.unwrap()
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string()
+        })
+        .collect::<Vec<String>>()
+}
+
+fn output_file(file_name: &str) -> String {
+    format!("prob-{}.sol", &file_name[5..8])
+}
+
 fn main() {
-    println!("W");
+    let matches = App::new("ICFPC 2019")
+        .version("0.1.0")
+        .arg(
+            Arg::with_name("input")
+                .long("input")
+                .takes_value(true)
+                .help("input root directory"),
+        )
+        .arg(
+            Arg::with_name("output")
+                .long("output")
+                .takes_value(true)
+                .help("output directory"),
+        )
+        .get_matches();
+
+    let input_root = matches.value_of("input").expect("no input specified");
+    let output_root = matches.value_of("output").expect("no output specified");
+
+    let files = find_files(&input_root);
+    for f in files {
+        let input_path = format!("{}/{}", input_root, f);
+        let output_path = format!("{}/{}", output_root, output_file(&f));
+        let mut file = File::create(output_path).unwrap();
+        writeln!(file, "W");
+    }
 }
