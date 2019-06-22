0ff6ddf4502209ffea7a4bed7cb9e19d3f485b3e
ichyo <ichyo00@gmail.com>
2019-06-22 15:56:53 +0900

    Refactoring

--- src/solve.rs
+++ src/solve.rs
@@ -9,8 +9,101 @@ trait Solver {
     fn solve(task: Task) -> Vec<Command>;
 }
 
-pub fn solve_small(task: Task) -> Vec<Command> {
+fn find_shortest_path(
+    width: usize,
+    height: usize,
+    valid: &Matrix<bool>,
+    passed: &Matrix<bool>,
+    start: Point,
+    bodies_diff: &[Point],
+    booster_map: &Matrix<Option<BoosterType>>,
+) -> Vec<Move> {
     let mut rng = thread_rng();
+    let mut moves = [
+        Move::MoveUp,
+        Move::MoveDown,
+        Move::MoveLeft,
+        Move::MoveRight,
+    ];
+
+    let mut data: Matrix<Option<(Move, u16)>> = Matrix::new(width, height, None);
+    let mut queue = VecDeque::new();
+    queue.push_back(start);
+    data.set(start, Some((Move::Noop, 0)));
+    while let Some(c) = queue.pop_front() {
+        let cost = match data.get(c) {
+            Some(Some((_, cost))) => *cost,
+            _ => panic!("no data is expected"),
+        };
+
+        let not_passed = bodies_diff
+            .iter()
+            .map(|diff| c + *diff)
+            .any(|p| match passed.get(p) {
+                Some(false) => true,
+                _ => false,
+            });
+        let is_booster = match booster_map.get(c) {
+            Some(Some(BoosterType::NewHand)) => true,
+            Some(Some(BoosterType::Drill)) => true,
+            _ => false,
+        };
+
+        if not_passed || is_booster {
+            let mut res = Vec::new();
+            let mut iter = c;
+            while iter != start {
+                let (mv, _cost) = match data.get(iter) {
+                    Some(Some((mv, cost))) => (mv, cost),
+                    _ => panic!("no data"),
+                };
+                iter = iter.revert_with(mv);
+                res.push(mv.clone());
+            }
+            res.reverse();
+            return res;
+        }
+
+        moves.shuffle(&mut rng);
+        for m in &moves {
+            let nc = c.move_with(m);
+            if let Some(None) = data.get(nc) {
+                if let Some(true) = valid.get(nc) {
+                    data.set(nc, Some((m.clone(), cost + 1)));
+                    queue.push_back(nc);
+                }
+            }
+        }
+    }
+    panic!("cannot reach anywhere");
+}
+
+fn update_point(
+    point: Point,
+    bodies_diff: &[Point],
+    passed: &mut Matrix<bool>,
+    booster_map: &mut Matrix<Option<BoosterType>>,
+    hand_count: &mut usize,
+    drill_count: &mut usize,
+    remaining: &mut usize,
+) {
+    bodies_diff.iter().map(|diff| point + *diff).for_each(|b| {
+        if let Some(false) = passed.get(b) {
+            passed.set(b, true);
+            *remaining -= 1;
+        }
+    });
+    if let Some(Some(kind)) = booster_map.get(point) {
+        match kind {
+            BoosterType::NewHand => *hand_count += 1,
+            BoosterType::Drill => *drill_count += 1,
+            _ => {}
+        }
+        booster_map.set(point, None);
+    }
+}
+
+pub fn solve_small(task: Task) -> Vec<Command> {
     let map_points = task.map.enumerate_points();
 
     let width = map_points.iter().map(|p| p.x).max().unwrap() as usize + 1;
@@ -41,18 +134,9 @@ pub fn solve_small(task: Task) -> Vec<Command> {
         }
     }
 
-    let mut moves = [
-        Move::MoveUp,
-        Move::MoveDown,
-        Move::MoveLeft,
-        Move::MoveRight,
-    ];
     let mut res = Vec::new();
-    let mut cp = task.initial;
-    if !passed.get(cp).unwrap() {
-        passed.set(cp, true);
-        remaining -= 1;
-    }
+    let mut current_point = task.initial;
+
     let mut bodies_diff = vec![
         Point::new(0, 0),
         Point::new(1, 1),
@@ -66,6 +150,7 @@ pub fn solve_small(task: Task) -> Vec<Command> {
         Point::new(0, -1),
         Point::new(0, 1),
     ]);
+
     let mut hand_count = 0;
     let mut drill_count = 0;
     let mut drill_turns = 0;
@@ -77,79 +162,40 @@ pub fn solve_small(task: Task) -> Vec<Command> {
             bodies_diff.push(new_hand);
             res.push(Command::NewHand(new_hand));
         }
-
-        let mut data: Matrix<Option<(Move, u16)>> = Matrix::new(width, height, None);
-        let mut queue = VecDeque::new();
-        queue.push_back(cp);
-        data.set(cp, Some((Move::Noop, 0)));
-        let mut reached = false;
-        while let Some(c) = queue.pop_front() {
-            let cost = match data.get(c) {
-                Some(Some((_, cost))) => *cost,
-                _ => panic!("no data is expected"),
-            };
-
-            let bodies = bodies_diff.iter().map(|diff| c + *diff).collect::<Vec<_>>();
-            let not_passed = bodies.iter().any(|p| {
-                if let Some(false) = passed.get(*p) {
-                    true
-                } else {
-                    false
-                }
-            });
-            let is_booster = match booster_map.get(c) {
-                Some(Some(BoosterType::NewHand)) => !new_bodies.is_empty(),
-                _ => false,
-            };
-            if not_passed || is_booster {
-                for body in bodies {
-                    if let Some(false) = passed.try_set(body, true) {
-                        remaining -= 1;
-                    }
-                }
-
-                let mut local_cmds = Vec::new();
-                let mut iter = c;
-                while iter != cp {
-                    match booster_map.get(iter) {
-                        Some(Some(BoosterType::NewHand)) => {
-                            hand_count += 1;
-                        }
-                        Some(Some(BoosterType::FastMove)) => {}
-                        Some(Some(BoosterType::Drill)) => {
-                            drill_count += 1;
-                        }
-                        _ => {}
-                    }
-                    booster_map.set(iter, None);
-
-                    let (mv, _cost) = match data.get(iter) {
-                        Some(Some((mv, cost))) => (mv, cost),
-                        _ => panic!("no data"),
-                    };
-                    iter = iter.revert_with(mv);
-                    local_cmds.push(Command::Move(mv.clone()));
-                }
-                local_cmds.reverse();
-                res.extend(local_cmds);
-
-                cp = c;
-                reached = true;
-                break;
-            }
-            moves.shuffle(&mut rng);
-            for m in &moves {
-                let nc = c.move_with(m);
-                if let Some(None) = data.get(nc) {
-                    if let Some(true) = valid.get(nc) {
-                        data.set(nc, Some((m.clone(), cost + 1)));
-                        queue.push_back(nc);
-                    }
-                }
-            }
+        if drill_count > 0 && drill_turns == 0 {
+            drill_count -= 1;
+            drill_turns = 30;
         }
-        if !reached {
-            panic!("cannot reach anywhere: remaining {}", remaining);
+        update_point(
+            current_point,
+            &bodies_diff,
+            &mut passed,
+            &mut booster_map,
+            &mut hand_count,
+            &mut drill_count,
+            &mut remaining,
+        );
+        let moves = find_shortest_path(
+            width,
+            height,
+            &valid,
+            &passed,
+            current_point,
+            &bodies_diff,
+            &booster_map,
+        );
+        for m in moves {
+            current_point = current_point.move_with(&m);
+            update_point(
+                current_point,
+                &bodies_diff,
+                &mut passed,
+                &mut booster_map,
+                &mut hand_count,
+                &mut drill_count,
+                &mut remaining,
+            );
+            res.push(Command::Move(m));
         }
     }
 
