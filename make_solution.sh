c89b9a209fbdf9b22e938679ff7e87c240920f2c
ichyo <ichyo00@gmail.com>
2019-06-23 20:10:34 +0900

    time in solution

--- make_solution.sh
+++ make_solution.sh
@@ -14,9 +14,11 @@ fi
 echo "$(date +"%Y-%m-%d %T"): Creating $OUTPUT" | tee -a $LOG_FILE
 mkdir -p $OUTPUT
 
+SECONDS=0
 ./target/release/icfpc2019 --output $OUTPUT --input ./input
+echo "$(date +"%Y-%m-%d %T"): running time is $SECONDS secs" | tee -a $LOG_FILE
 
-echo "$(date +"%Y-%m-%d %T"): Creating zip $OUTPUT/solutions.zip"
+echo "$(date +"%Y-%m-%d %T"): Creating zip $OUTPUT/solutions.zip" | tee - a $LOG_FILE
 cd $OUTPUT
 zip solutions.zip ./*.sol -q
 
