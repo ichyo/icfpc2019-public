f989008e5f2a4c5d65e4e9e836f7314a3a777c4d
ichyo <ichyo00@gmail.com>
2019-06-24 13:56:25 +0900

    Add make best

--- make_best.sh
+++ make_best.sh
@@ -13,7 +13,7 @@ cargo run --bin compare -- --output $OUTPUT --input ./input --file $LIST_FILE
 
 echo "$(date +"%Y-%m-%d %T"): Creating zip $OUTPUT/solutions.zip" | tee -a $LOG_FILE
 cd $OUTPUT
-zip solutions.zip ./*.sol -q
+zip solutions.zip ./*.sol ./*.buy -q
 
 cd -
 cargo run --bin score --release -- --input ./input --output $OUTPUT | tee -a $LOG_FILE
