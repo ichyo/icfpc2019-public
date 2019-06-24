a1308e8d58486fb3315166f0826416bbe37b98d5
ichyo <ichyo00@gmail.com>
2019-06-24 10:08:37 +0900

    Submit
    remote: 2421258 -> 2490564
    local: 3258712 -> 3345918

---
+++ make_best.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+LIST_FILE=./submissions.txt
+OUTPUT=./best_solution
+LOG_FILE=./best.log
+
+echo "$(date +"%Y-%m-%d %T"): Running make best" | tee -a $LOG_FILE
+cat $LIST_FILE | tee -a $LOG_FILE
+
+cargo run --bin compare -- --output $OUTPUT --input ./input --file $LIST_FILE
+
+echo "$(date +"%Y-%m-%d %T"): Creating zip $OUTPUT/solutions.zip" | tee -a $LOG_FILE
+cd $OUTPUT
+zip solutions.zip ./*.sol -q
+
+cd -
+cargo run --bin score --release -- --input ./input --output $OUTPUT | tee -a $LOG_FILE
+
